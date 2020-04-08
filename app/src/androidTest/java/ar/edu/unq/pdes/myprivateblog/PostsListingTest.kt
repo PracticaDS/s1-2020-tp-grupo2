b378f857e0220ad0b586e7c694712198674d3090
Ariel Alvarez <ariel@lightvessel.org>
2020-04-08 19:18:00 -0300

    Adds test for post creation

--- app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostsListingTest.kt
+++ app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostsListingTest.kt
@@ -2,9 +2,9 @@ package ar.edu.unq.pdes.myprivateblog
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withHint
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -26,7 +26,30 @@ class PostsListingTest {
         onView(withId(R.id.create_new_post))
             .perform(click())
 
+
         onView(withId(R.id.title))
-            .check(matches(withHint("My Post Title")))
+            .check(matches(withHint(R.string.hint_post_title)))
     }
+
+    @Test
+    fun whenCreatingPost_shouldNavigateToPostDetail() {
+        onView(withId(R.id.create_new_post))
+            .perform(click())
+
+        val postTitle = "post1"
+
+        onView(withId(R.id.title))
+            .perform(typeText(postTitle))
+
+        onView(withId(R.id.body))
+            .perform(typeText("This is the body"))
+
+        onView(withId(R.id.btn_save)).perform(click())
+
+        onView(withId(R.id.title))
+            .check(matches(withText(postTitle)))
+
+    }
+
+
 }
